b1bcfe4161d8b4e70e08585e287d7d73fac3a201
Nsocha <nsocha@godaddy.com>
2022-06-20 16:09:39 +0530

    search result page structure

--- js/general.js
+++ js/general.js
@@ -115,6 +115,12 @@
 				}
 			});
 		}
+
+		var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+			var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  			return new bootstrap.Tooltip(tooltipTriggerEl)
+		})
+
 		
 /*--------------------------------------------------------------------------------------------------------------------------------------*/		
 	});	
