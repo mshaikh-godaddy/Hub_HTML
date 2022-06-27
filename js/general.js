2387b88abecc2a9dcebbaaf7f3764fc89c431999
Nsocha <nsocha@godaddy.com>
2022-06-27 10:07:05 +0530

    slider jquery update

--- js/general.js
+++ js/general.js
@@ -31,16 +31,16 @@
 	$doc.ready(function() {
 /*--------------------------------------------------------------------------------------------------------------------------------------*/		
 	
-		var swiper = new Swiper(".tld-slider", {
+		/*var swiper = new Swiper(".tld-slider", {
 			loop:true,
-			freeMode: true,
-			slidesPerView: 8.6,
-			spaceBetween: 0,
-			centeredSlides: false,
+			spaceBetween: 6,
+			slidesPerView: 1,
 			direction: "vertical",
-			mousewheel: true,
-			keyboard: true,
-			watchSlidesProgress: true,
+			//centeredSlides: true,
+			// watchOverflow: true,
+			// watchSlidesVisibility: true,
+			// watchSlidesProgress: true,
+			slideToClickedSlide: true,
 			thumbs: {
 				swiper: swiper2,
 			},
@@ -48,24 +48,24 @@
 				nextEl: ".tld-button-next",
 				prevEl: ".tld-button-prev",
 			},
-			breakpoints: {
-				0: {
-					slidesPerView: 5,
-					spaceBetween: 6,
-				},
-				768: {
-					slidesPerView: 9,
-					spaceBetween: 8,
-				},
-				992: {
-					slidesPerView: 13.1,
-					spaceBetween: 8,
-				},
-				1300: {
-					slidesPerView: 14,
-					spaceBetween: 8,
-				},
-			}
+			// breakpoints: {
+			// 	0: {
+			// 		slidesPerView: 5,
+			// 		spaceBetween: 6,
+			// 	},
+			// 	768: {
+			// 		slidesPerView: 9,
+			// 		spaceBetween: 8,
+			// 	},
+			// 	992: {
+			// 		slidesPerView: 13.1,
+			// 		spaceBetween: 8,
+			// 	},
+			// 	1300: {
+			// 		slidesPerView: 14,
+			// 		spaceBetween: 8,
+			// 	},
+			// }
 		});
 		var swiper2 = new Swiper(".home-slider", {
 			loop: true,
@@ -77,10 +77,12 @@
 				prevEl: ".tld-button-prev",
 			},
 			thumbs: {
-			  swiper: swiper,
+				swiper: swiper,
 			},
 		});
 
+		//swiper.controller.control = swiper2;
+		//swiper2.controller.control = swiper;
 
 		/*TLD category slider
 		---------------------------------------------------------------------*/
@@ -115,6 +117,61 @@
 				}
 			});
 		}
+
+		var main = new Splide( '.main-slider', {
+			type   : 'fade',
+			heightRatio: 0.5,
+			pagination : false,
+			arrows     : false,
+			cover      : true,
+		} );
+		  
+		var thumbnails = new Splide( '.thumbnail-slider', {
+			type   : 'loop',
+			isNavigation    : true,
+			gap             : 0,
+			focus           : 1,
+			pagination      : false,
+			cover           : true,
+			direction       : 'ttb',
+			heightRatio : 3.1,
+			perPage: 15,
+			// dragMinThreshold: {
+			//   mouse: 4,
+			//   touch: 10,
+			// },
+			breakpoints : {
+				419: {
+					heightRatio : 1.5,
+					perPage: 6,
+				},
+				575: {
+					heightRatio : 1.6,
+					perPage: 7,
+				},
+				767: {
+					heightRatio : 1.6,
+					perPage: 7,
+				},
+				991: {
+					heightRatio : 2,
+					perPage: 10,
+				},
+				1199: {
+					heightRatio : 2.2,
+					perPage: 14,
+				},
+				1399: {
+					heightRatio : 2.3,
+					perPage: 14,
+				},
+			},
+		} );
+		  
+		main.sync( thumbnails );
+		main.mount();
+		thumbnails.mount();
+		
 
 		var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 			var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
